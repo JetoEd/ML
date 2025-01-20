9de44cadd93ea1e7cc210025e9ffd3cfbd8450b4
JetoEd <jetoed9182@gmail.com>
2025-01-19 23:11:16 -0500

    just further testing

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential 
 from tensorflow.keras.layers import Dense
-# from tensorflow.python.keras.losses import BinaryCrossentropy #Logistic loss
-# from tensorflow.python.keras.losses import MeanSquaredError #linear regression loss 
+from tensorflow.keras.losses import BinaryCrossentropy #Logistic loss
+from tensorflow.keras.losses import MeanSquaredError #linear regression loss 
 from tensorflow.keras.losses import SparseCategoricalCrossentropy #softmax loss function 
 from tensorflow.keras.optimizers import Adam
 
@@ -13,10 +13,12 @@ model = Sequential([
 ])
  
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss= SparseCategoricalCrossentropy(from_logits=True))
-#model.compile(loss= MeanSquaredError())
+model.compile(loss= MeanSquaredError())
 
 # model.fit(X, y, epochs=100)
 
+
+# print(tf.__version__)
 
 
 
